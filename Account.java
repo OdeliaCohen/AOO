9be1d3a340ffb68ef7a7d044a7019a4bc07c3314
OdeliaCohen <odeliacohen1503@gmail.com>
2024-03-07 15:05:59 +0100

    class & attributs

---
+++ Account.java
@@ -0,0 +1,12 @@
+public class Account {
+
+    private String id;
+    private String password;
+    private String email;
+    private String name;
+    private String lastname;
+    private String age;
+
+
+    
+}
