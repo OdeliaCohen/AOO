ce64df92cc73eecc3fa18bfd36b517c1588f50c1
OdeliaCohen <odeliacohen1503@gmail.com>
2024-03-07 15:28:00 +0100

    methode

--- Account.java
+++ Account.java
@@ -7,6 +7,29 @@ public class Account {
     private String lastname;
     private int age;
 
+    public Account(String id, String password, String email, String name, String lastname, int age) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public void updateAccount(String id, String password, String email, String name, String lastname, int age) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public Account signUp(String id, String password, String email, String name, String lastname, int age) {
+        return new Account(id, password, email, name, lastname, age);
+    }
+
+
 
     
 }
--- ExpensesCategory.java
+++ ExpensesCategory.java
@@ -1,6 +1,16 @@
+
 public class ExpensesCategory {
 
     private String id;
     private String categoryName;
+
+    public ExpensesCategory(String id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public ExpensesCategory createCategory(String id, String categoryName) {
+        return new ExpensesCategory(id, categoryName);
+    }
     
 }
